490c53213d642c21812849089d942789358f9739
Hasan Basri <hasanbasri1493@gmail.com>
2026-05-13 22:35:29 +0700

    chore: migrate Expo configuration to JavaScript and add expo-modules dependencies

---
+++ app.config.js
@@ -0,0 +1,100 @@
+/** @type {import('expo/config').ExpoConfig} */
+module.exports = ({ config }) => ({
+  name: 'Chatwoot Dulido',
+  slug: process.env.EXPO_PUBLIC_APP_SLUG || 'chatwoot-mobile',
+  version: '4.5.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
+  newArchEnabled: false,
+  scheme: 'chatwootapp',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+    enableFullScreenImage_legacy: true,
+  },
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'com.daarululuumlido.chatwoot',
+    infoPlist: {
+      NSCameraUsageDescription: 'This app requires access to the camera to upload images and videos.',
+      NSPhotoLibraryUsageDescription: 'This app requires access to the photo library to upload images.',
+      NSMicrophoneUsageDescription: 'This app requires access to the microphone to record audio.',
+      NSAppleMusicUsageDescription: 'This app does not use Apple Music, but a system API may require this permission.',
+      UIBackgroundModes: ['fetch', 'remote-notification'],
+      ITSAppUsesNonExemptEncryption: false,
+    },
+    googleServicesFile: './GoogleService-Info.plist',
+    entitlements: {
+      'aps-environment': 'production',
+    },
+    associatedDomains: ['applinks:chatwoot.daarululuumlido.com'],
+  },
+  android: {
+    package: 'com.daarululuumlido.chatwoot',
+    permissions: [
+      'android.permission.CAMERA',
+      'android.permission.READ_EXTERNAL_STORAGE',
+      'android.permission.WRITE_EXTERNAL_STORAGE',
+      'android.permission.RECORD_AUDIO',
+      'android.permission.READ_MEDIA_IMAGES',
+    ],
+    googleServicesFile: './google-services.json',
+    intentFilters: [
+      {
+        action: 'VIEW',
+        autoVerify: true,
+        data: [
+          {
+            scheme: 'https',
+            host: 'chatwoot.daarululuumlido.com',
+            pathPrefix: '/app/accounts/',
+            pathPattern: '/*/conversations/*',
+          },
+        ],
+        category: ['BROWSABLE', 'DEFAULT'],
+      },
+      {
+        action: 'VIEW',
+        data: [{ scheme: 'chatwootapp' }],
+        category: ['BROWSABLE', 'DEFAULT'],
+      },
+    ],
+  },
+  extra: {
+    eas: {
+      projectId: 'cdf619a0-5a45-448c-acf2-41213cc19781',
+      storybookEnabled: process.env.EXPO_STORYBOOK_ENABLED,
+    },
+  },
+  owner: 'hasanbasri93',
+  plugins: [
+    'expo-font',
+    ['react-native-permissions', { iosPermissions: ['Camera', 'PhotoLibrary', 'MediaLibrary'] }],
+    [
+      '@sentry/react-native/expo',
+      {
+        url: 'https://sentry.io/',
+        project: 'chatwoot',
+        organization: 'hasan93',
+      },
+    ],
+    '@react-native-firebase/app',
+    '@react-native-firebase/messaging',
+    [
+      'expo-build-properties',
+      {
+        android: {
+          minSdkVersion: 24,
+          compileSdkVersion: 35,
+          targetSdkVersion: 35,
+          enableProguardInReleaseBuilds: true,
+        },
+        ios: { useFrameworks: 'static' },
+      },
+    ],
+    './with-ffmpeg-pod.js',
+  ],
+  androidNavigationBar: { backgroundColor: '#ffffff' },
+});
